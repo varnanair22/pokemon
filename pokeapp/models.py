e1e1b8acf84205add433e004036d264654030a00
varnanair22 <mbamanojm@gmail.com>
2021-02-19 19:19:16 +0530

    admin module to edit and remove pokemon

--- pokeapp/models.py
+++ pokeapp/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 # Create your models here.
+
+class pokemon(models.Model):
+    name = models.CharField(max_length=250)
+    url = models.CharField(max_length=1000)
+    desc = models.CharField(max_length=500, default="", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
