3382e01a1cae9fef2365b3d7799554685c3a20e6
varnanair22 <mbamanojm@gmail.com>
2021-02-20 18:48:01 +0530

    api to add pokemon

--- pokeapp/views.py
+++ pokeapp/views.py
@@ -10,12 +10,18 @@ def add_pokemon(request):
     if request.method =="POST":
         if request.POST.get('name') and request.POST.get('url'):
             pokemon_inst = pokemon()
-            pokemon_inst.name = request.POST.get('name')
-            pokemon_inst.url = request.POST.get('url')
-            pokemon_inst.save()
-            return HttpResponse("Successfully added!!!!")
+            if pokemon.objects.all().filter(name__icontains=request.POST.get('name')).exists():
+                return HttpResponse("Pokemon with same name already existing!!!!")
+            else:
+                pokemon_inst.name = request.POST.get('name')
+                pokemon_inst.url = request.POST.get('url')
+                pokemon_inst.save()
+                return HttpResponse("Successfully added!!!!")
         else:
             return HttpResponse("Parameters missing!!!!")
+    else:
+        return render(request, 'add_pokemon.html')
+
 
 def get_pokemon(request):
     pokemons = pokemon.objects.all()
