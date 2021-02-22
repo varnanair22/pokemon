482bbd239c7c5bfeb561c5f59746033fcb6d2c1d
varnanair22 <mbamanojm@gmail.com>
2021-02-23 02:45:48 +0530

    readme.md, requirements.txt and add pokemon via api'

---
+++ README.md
@@ -0,0 +1,28 @@
+The first thing to do is to clone the repository:
+
+$ git clone https://github.com/varnanair22/pokemon.git
+$ cd pokemon_app
+
+Create a virtual environment to install dependencies in and activate it:
+
+$ virtualenv2 --no-site-packages env
+$ source env/bin/activate
+
+Then install the dependencies:
+
+(env)$ pip install -r requirements.txt
+Note the (env) in front of the prompt. This indicates that this terminal session operates in a virtual environment set up by virtualenv2.
+
+Once pip has finished downloading the dependencies:
+
+(env)$ cd pokemon
+(env)$ python manage.py runserver
+And navigate to http://127.0.0.1:8000/ for listing the pokemons.
+
+http://127.0.0.1:8000/admin can be used to add, edit and delete pokemons using admin module.
+Credentials:
+Username - amdin
+Password - admin123
+
+http://127.0.0.1:8000/add_pokemon used for adding pokemons via api.
+Enter the api, based on its response pokemon details will be added in database avoiding duplicacy.
--- pokeapp/views.py
+++ pokeapp/views.py
@@ -1,26 +1,45 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import pokemon
-
+import requests
 
 # Create your views here.
 
 
+#def add_pokemon(request):
+#    if request.method =="POST":
+#        if request.POST.get('name') and request.POST.get('url'):
+#            pokemon_inst = pokemon()
+#            if pokemon.objects.all().filter(name__icontains=request.POST.get('name')).exists():
+#                return HttpResponse("Pokemon with same name already existing!!!!")
+#            else:
+#                pokemon_inst.name = request.POST.get('name')
+#                pokemon_inst.url = request.POST.get('url')
+#                pokemon_inst.save()
+#                return HttpResponse("Successfully added!!!!")
+#        else:
+#            return HttpResponse("Parameters missing!!!!")
+#    else:
+#        return render(request, 'add_pokemon.html')
+
+
 def add_pokemon(request):
-    if request.method =="POST":
-        if request.POST.get('name') and request.POST.get('url'):
-            pokemon_inst = pokemon()
-            if pokemon.objects.all().filter(name__icontains=request.POST.get('name')).exists():
-                return HttpResponse("Pokemon with same name already existing!!!!")
-            else:
-                pokemon_inst.name = request.POST.get('name')
-                pokemon_inst.url = request.POST.get('url')
-                pokemon_inst.save()
-                return HttpResponse("Successfully added!!!!")
-        else:
-            return HttpResponse("Parameters missing!!!!")
+    if request.method == "POST":
+        api = request.POST.get('api')
+        response = requests.get(api)
+        data = response.json()
+        pokemons_lst = data['results']
+
+        for i in pokemons_lst:
+            if not pokemon.objects.all().filter(name__icontains=i['name']).exists():
+                pokemon_data = pokemon(
+                    name=i['name'],
+                    url=i['url'],
+                )
+                pokemon_data.save()
+        return HttpResponse("Successfully added!!!!")
     else:
-        return render(request, 'add_pokemon.html')
+        return render(request, 'add_pokemon_api.html')
 
 
 def get_pokemon(request):
